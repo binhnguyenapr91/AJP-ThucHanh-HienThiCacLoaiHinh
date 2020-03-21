d7788366b8efc0ec1f649b90ee9f9f82f9bc729a
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-03-21 16:00:24 +0700

    fix

--- src/App.java
+++ src/App.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 
 public class App {
-
+    static int width;
+    static int height;
     void showMenu() {
-        System.out.println("---MENU---");
+        System.out.println("----------MENU-------------");
         System.out.println("1.Print rectangle");
         System.out.println("2.Print the square triangle");
         System.out.println("3.Print isosceles triangle");
-        System.out.println("----------");
+        System.out.println("---------------------------");
         System.out.println("0.Exit");
+        System.out.println("---------------------------");
     }
 
     void showMenu2() {
         System.out.println("---Select type of Square Triangle---");
         System.out.println("1.Bottom-left");
         System.out.println("2.Top-left");
-        System.out.println("----------");
+        System.out.println("------------------------------------");
         System.out.println("0.Back");
+        System.out.println("------------------------------------");
     }
 
     public static void main(String[] args) {
@@ -30,13 +33,21 @@ public class App {
                     case 0:
                         System.exit(0);
                     case 1:
-                        Rectangle rec = new Rectangle(8, 4);
+                        System.out.println("Enter width:");
+                        width = scanner.nextInt();
+                        System.out.println("Enter height:");
+                        height = scanner.nextInt();
+                        Rectangle rec = new Rectangle(width, height);
                         rec.draw();
                         break;
                     case 2:
                         int choice2 = 100;
+                        System.out.println("Enter width:");
+                        width = scanner.nextInt();
+                        System.out.println("Enter height:");
+                        height = scanner.nextInt();
                         while (choice2 != 0) {
-                            SquareTriangle sqTriangle = new SquareTriangle(9, 9);
+                            SquareTriangle sqTriangle = new SquareTriangle(width,height);
                             app.showMenu2();
                             choice2 = scanner.nextInt();
                             switch (choice2) {
@@ -55,7 +66,11 @@ public class App {
                         }
                         break;
                     case 3:
-                        IsoscelesTriangle islTriangle = new IsoscelesTriangle(11,6);
+                        System.out.println("Enter width:");
+                        width = scanner.nextInt();
+                        System.out.println("Enter height:");
+                        height = scanner.nextInt();
+                        IsoscelesTriangle islTriangle = new IsoscelesTriangle(width,height);
                         islTriangle.draw();
                         break;
                     default:
